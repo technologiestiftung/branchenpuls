a782a2414351fa942fa9ca53f7f90c3f5402ec6a
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-07-03 15:04:57 +0200

    Housekeeping: Move files from /app dir to toplevel, always use prod database

---
+++ src/lib/db.js
@@ -0,0 +1,19 @@
+const pgp = require("pg-promise")();
+// const devMode = process.env.NODE_ENV === "development";
+// const settingsLocal = {
+//   host: process.env.NEXT_PUBLIC_DB_HOST_LOCAL,
+//   port: process.env.NEXT_PUBLIC_DB_PORT_LOCAL,
+//   database: process.env.NEXT_PUBLIC_DB_DATABASE_LOCAL,
+//   user: process.env.NEXT_PUBLIC_DB_USER_LOCAL,
+//   password: process.env.NEXT_PUBLIC_DB_PASSWORD_LOCAL,
+// };
+const settingProduction = {
+  host: process.env.NEXT_PUBLIC_DB_HOST,
+  port: process.env.NEXT_PUBLIC_DB_PORT,
+  database: process.env.NEXT_PUBLIC_DB_DATABASE,
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+};
+const db = pgp(settingProduction);
+
+module.exports = db;
