14ae8c15d0b348f1a35ec4e63237d653f34a1d5c
hhack <hans.f.hack@googlemail.com>
2023-07-13 13:25:02 +0200

    add constum style

--- src/lib/selectStyles.js
+++ src/lib/selectStyles.js
@@ -1,13 +1,11 @@
-export function customStyles(theme) {
-	return {
-		placeholder: (baseStyles, state) => ({
-			...baseStyles,
-			color: "#dadada",
-			fontSize: "0.875rem",
-			fontStyle: "italic",
-		}),
-	};
-}
+export const customStyles = {
+	placeholder: (baseStyles, state) => ({
+		...baseStyles,
+		color: "#dadada",
+		fontSize: "0.875rem",
+		fontStyle: "italic",
+	}),
+};
 
 export function customTheme(theme) {
 	return {
