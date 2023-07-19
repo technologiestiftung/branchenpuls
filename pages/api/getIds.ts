b8a5a9b998c159e23caa8c148255412b1518e195
hhack <hans.f.hack@googlemail.com>
2023-07-19 13:38:32 +0200

    turn to array if string in api

--- pages/api/getIds.ts
+++ pages/api/getIds.ts
@@ -42,14 +42,23 @@ export default async function handler(
 	}
 
 	if (bl1) {
+		if (bl1.includes(",")) {
+			bl1 = bl1?.split(",").map((d) => Number(d.trim()));
+		}
 		query = query.in("branch_top_level_id", Array.isArray(bl1) ? bl1 : [bl1]);
 	}
 
 	if (bl2) {
+		if (bl2.includes(",")) {
+			bl2 = bl2?.split(",").map((d) => Number(d.trim()));
+		}
 		query = query.in("nace_id", Array.isArray(bl2) ? bl2 : [bl2]);
 	}
 
 	if (bl3) {
+		if (bl3.includes(",")) {
+			bl3 = bl3?.split(",").map((d) => Number(d.trim()));
+		}
 		query = query.in("ihk_branch_id", Array.isArray(bl3) ? bl3 : [bl3]);
 	}
 
