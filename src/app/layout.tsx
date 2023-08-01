d0f32aa1f25de17664760192395153f9f0ea0ede
Lisa-Stubert <lisastubert@web.de>
2023-08-01 12:38:53 +0200

    feat: add metadata description and title for social media

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,25 @@ import "./globals.css";
 
 // https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadata-fields
 export const metadata = {
-	title: "Branchen Puls",
-	description: "Gewerbedaten von Berlin erkunden",
+	title: "Berlins Branchenpuls",
+	description:
+		"Berlins Wirtschaft ist so bunt wie die Stadt selbst. Mit dem Branchenpuls tauchst du in die dynamische Berliner Gewerbelandschaft ein. Exploriere und vergleiche Berlins Gewerbe vom Bezirk bis zum Kiez.",
 	keywords: ["ODIS", "Technologiestiftung Berlin", "CityLAB", "IHK Berlin"],
 	locale: "de_DE",
 	type: "website",
 	themeColor: "#1e398f",
 	twitter: {
 		card: "summary_large_image",
-		title: "Branchen Puls",
-		description: "Gewerbedaten von Berlin erkunden",
+		title: "Berlins Branchenpuls",
+		description:
+			"Berlins Wirtschaft ist so bunt wie die Stadt selbst. Mit dem Branchenpuls tauchst du in die dynamische Berliner Gewerbelandschaft ein. Exploriere und vergleiche Berlins Gewerbe vom Bezirk bis zum Kiez.",
 		// creator: "@nextjs",
 		images: ["https://nextjs.org/og.png"],
 	},
 	openGraph: {
-		title: "Branchen Puls",
-		description: "Gewerbedaten von Berlin erkunden",
+		title: "Berlins Branchenpuls",
+		description:
+			"Berlins Wirtschaft ist so bunt wie die Stadt selbst. Mit dem Branchenpuls tauchst du in die dynamische Berliner Gewerbelandschaft ein. Exploriere und vergleiche Berlins Gewerbe vom Bezirk bis zum Kiez.",
 		url: "https://nextjs.org",
 		siteName: "Next.js",
 		images: [
